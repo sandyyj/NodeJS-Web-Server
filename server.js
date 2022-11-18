a07fc5d5e63272a44b1f182652dfcf0b1508c16c
sandyyj <117791123+sandyyj@users.noreply.github.com>
2022-11-18 09:24:47 +0700

    Add server.js and package.json

---
+++ server.js
@@ -0,0 +1,69 @@
+const http = require('http')
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'application/json')
+    response.setHeader('X-Powered-By', 'NodeJS')
+    const { url, method } = request
+    // if (method == "GET") {
+    //     response.end('Hello')
+    // }
+    // if (method == "POST") {
+    //     let body = []
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk)
+    //     })
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString()
+    //         const name = JSON.parse(body)
+    //         response.end(`Hai, ${body}`)
+    //     })
+    // }
+    // if (method == "POST") {
+    //     response.end('Bonjour')
+    // }
+    if (url == "/") {
+        if (method == "GET") {
+            response.statusCode = 200
+            response.end("Ini adalah homepage")
+        }
+        else {
+            response.statusCode = 400
+            response.end('Halaman tidak dapat diakses dengan <any> request')
+        }
+    }
+    else if (url == "/about") {
+        if (method == "GET") {
+            response.statusCode = 200
+            response.end("Halo! ini adalah halaman about")
+        }
+        else if (method == "POST") {
+            let body = []
+            request.on('data', (chunk) => {
+                body.push(chunk)
+            })
+            request.on('end', () => {
+                body = Buffer.concat(body).toString()
+                const { name } = JSON.parse(body)
+                response.statusCode = 200
+                response.end(`Halo, ${name}! Ini adalah halaman about`)
+            })
+        }
+        else {
+            response.statusCode = 400
+            response.end("Halaman tidak dapat diakses dengan <any> request")
+        }
+    }
+    else {
+        response.statusCode = 404
+        response.end("Halaman tidak ditemukan!")
+    }
+}
+
+const server = http.createServer(requestListener)
+
+const port = 5000
+const host = 'localhost'
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`)
+})
